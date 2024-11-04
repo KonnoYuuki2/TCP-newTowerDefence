eb427c30fc325ca6b4b5e94189a47fdbb09bd80c
ohyudan <ohyudan86@gmail.com>
2024-11-04 15:23:31 +0900

    fix:env.js

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,14 +1,26 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const envFiles = {
   Server: {
     PORT: process.env.PORT || 5555,
-    HOST: process.env.HOST || "localhost",
+    HOST: process.env.HOST || 'localhost',
   },
   Client: {
     VERSION: process.env.VERSION,
+  },
+  User_DB: {
+    DB_NAME: process.env.DB1_NAME,
+    DB_USER: process.env.DB1_USER,
+    DB_PASSWORD: process.env.DB1_SERVER_PASSWORD,
+    DB_PORT: process.env.DB1_SERVER_PORT,
+  },
+  GamePlay_DB: {
+    DB_NAME: process.env.DB2_NAME,
+    DB_USER: process.env.DB2_USER,
+    DB_PASSWORD: process.env.DB2_SERVER_PASSWORD,
+    DB_PORT: process.env.DB2_SERVER_PORT,
   },
 };
 
