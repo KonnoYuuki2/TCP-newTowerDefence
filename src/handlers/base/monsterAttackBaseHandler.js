d0af023fa0fa3629cdae688894312e48ffe22b35
MinjaeK <93378322+KonnoYuuki2@users.noreply.github.com>
2024-11-06 00:58:33 +0900

    Update: baseHandler
    baseHandler 추가 및 baseUtils.js 추가

---
+++ src/handlers/base/monsterAttackBaseHandler.js
@@ -0,0 +1,13 @@
+import { monsterAttackBaseHpVerify } from '../../utils/base/baseUtils.js';
+
+const monsterAttackBaseHandler = async (socket, payload) => {
+  try {
+    const { damage } = payload;
+
+    const user = await getUserBySocket(socket);
+
+    await monsterAttackBaseHpVerify(damage, user);
+  } catch (error) {
+    throw new Error(`몬스터 베이스 어택 처리 중 에러 발생`, error);
+  }
+};
---
+++ src/utils/base/baseUtils.js
@@ -0,0 +1,20 @@
+import pools from '../../DB/dataBase.js';
+
+export const getBaseHp = async (user) => {
+  const user = await pools.GAME_DATABASE_REDIS.hget(`user: ${user.id}`);
+
+  return user.baseHp;
+};
+
+export const monsterAttackBaseHpVerify = async (damage, user) => {
+  const baseHp = await getBaseHp(user);
+
+  if (!baseHp) {
+    throw new Error(`baseHp가 존재하지 않습니다.`);
+  }
+
+  baseHp -= damage;
+
+  // 유저 정보 업데이트 해주기
+  await pools.GAME_DATABASE_REDIS.hset(...user, baseHp);
+};
