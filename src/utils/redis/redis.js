0890f545a3280226c7cfd4dc2c1317ded4d6f3b6
박지영 <124862122+shd1495@users.noreply.github.com>
2024-11-06 15:30:39 +0900

    fix: 세션에 유저 저장 방식 변경
    데이터 삽입시 TTL 추가
    유저의 인게임 데이터 중 한 필드만
    조회하는 기능 추가

--- src/utils/redis/redis.js
+++ src/utils/redis/redis.js
@@ -4,51 +4,38 @@ const redisClient = pools.GAME_DATABASE_REDIS;
 
 // Prefix 상수
 const USER_PREFIX = 'user';
-const USERS_PREFIX = 'users';
 const GAME_SESSION_PREFIX = 'gameSession';
 
 export const redis = {
   // 게임 세션에 유저 추가
-  addUser: async (gameId, user) => {
-    const userKey = `${GAME_SESSION_PREFIX}:${gameId}:${USER_PREFIX}:${user.id}`;
-    await redisClient.hset(userKey, {
-      socketId: user.socket.id,
-      userId: user.id,
-    });
-    await redisClient.sadd(`${GAME_SESSION_PREFIX}:${gameId}:${USERS_PREFIX}`, user.id);
+  addUser: async (gameId, userId) => {
+    const key = `${GAME_SESSION_PREFIX}:${gameId}`;
+    await redisClient.sadd(key, userId);
+    await redisClient.expire(key, 3600);
   },
 
   // 게임 세션에서 유저 제거
-  removeUser: async (gameId, socket) => {
-    const userId = await redisClient.hget(
-      `${GAME_SESSION_PREFIX}:${gameId}:socketToUser`,
-      socket.id,
-    );
-    if (userId) {
-      const userKey = `${GAME_SESSION_PREFIX}:${gameId}:${USER_PREFIX}:${userId}`;
-      const userData = await redisClient.hgetall(userKey);
-      await redisClient.del(userKey);
-      await redisClient.srem(`${GAME_SESSION_PREFIX}:${gameId}:${USERS_PREFIX}`, userId);
-      return userData;
-    }
-    return null;
+  removeUser: async (gameId, userId) => {
+    await redisClient.srem(`${GAME_SESSION_PREFIX}:${gameId}`, userId);
+  },
+
+  // 게임 세션의 모든 유저 조회
+  getUsers: async (gameId) => {
+    const users = await redisClient.smembers(`${GAME_SESSION_PREFIX}:${gameId}`);
+    return users;
   },
 
   // 게임 세션에서 유저 조회
   getUser: async (gameId, userId) => {
-    const userKey = `${GAME_SESSION_PREFIX}:${gameId}:${USER_PREFIX}:${userId}`;
-    return await redisClient.hgetall(userKey);
+    const exists = await redisClient.sismember(`${GAME_SESSION_PREFIX}:${gameId}`, userId);
+    return exists === 1 ? userId : null;
   },
 
-  // 게임 세션의 유저 수 조회
-  getGameUserCount: async (gameId) => {
-    return await redisClient.scard(`${GAME_SESSION_PREFIX}:${gameId}:${USERS_PREFIX}`);
-  },
-
-  // 유저 게임 데이터 관리
+  // 유저 게임 데이터 설정
   setUserData: async (userId, userData) => {
+    const key = `${USER_PREFIX}:${userId}`;
     try {
-      await redisClient.hset(`${USER_PREFIX}:${userId}`, {
+      await redisClient.hset(key, {
         userGold: userData.userGold,
         baseHp: userData.baseHp,
         towerData: JSON.stringify(userData.towerData),
@@ -56,11 +43,13 @@ export const redis = {
         level: userData.level,
         score: userData.score,
       });
+      await redisClient.expire(key, 3600);
     } catch (error) {
       console.error(`유저 데이터 저장 중 에러 발생: ${error}`);
     }
   },
 
+  // 유저 게임 데이터 조회
   getUserData: async (userId) => {
     try {
       const data = await redisClient.hgetall(`${USER_PREFIX}:${userId}`);
@@ -79,15 +68,37 @@ export const redis = {
     }
   },
 
+  getUserField: async (userId, field) => {
+    try {
+      const data = await redisClient.hget(`${USER_PREFIX}:${userId}`, field);
+      if (!data) return null;
+
+      // 필드에 따라 형 변환
+      if (['userGold', 'baseHp', 'level', 'score'].includes(field)) {
+        return parseInt(data);
+      } else if (['towerData', 'monsterData'].includes(field)) {
+        return JSON.parse(data);
+      }
+
+      return data;
+    } catch (error) {
+      console.error(`유저 필드 조회 중 에러 발생: ${error}`);
+    }
+  },
+
+  // 유저 게임 데이터 수정
   updateUserField: async (userId, field, value) => {
+    const key = `${USER_PREFIX}:${userId}`;
     try {
       const stringValue = typeof value === 'object' ? JSON.stringify(value) : value;
-      await redisClient.hset(`${USER_PREFIX}:${userId}`, field, stringValue);
+      await redisClient.hset(key, field, stringValue);
+      await redisClient.expire(key, 3600);
     } catch (error) {
       console.error(`유저 필드 업데이트 중 에러 발생: ${error}`);
     }
   },
 
+  // 유저 게임 데이터 삭제
   deleteUserData: async (userId) => {
     try {
       await redisClient.del(`${USER_PREFIX}:${userId}`);
