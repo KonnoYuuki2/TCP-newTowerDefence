e2d5bd1cf3fb9d99d55a0d44c44499a0dab69ab9
ohyudan <ohyudan86@gmail.com>
2024-11-06 16:24:53 +0900

    fix: user.db.js

--- src/DB/user/user.db.js
+++ src/DB/user/user.db.js
@@ -1,17 +1,18 @@
 //import { v4 as uuidv4 } from 'uuid';
 import pools from '../dataBase.js';
 import { SQL_QUERIES } from './user.queries.js';
-//import { toCamelCase } from '../../utils/transformCase.js';
 
-// export const findUserByDeviceID = async (deviceId) => {
-//   const [rows] = await pools.USER_DATABASE_SQL.query(SQL_QUERIES.FIND_USER_BY_DEVICE_ID, [
-//     deviceId,
-//   ]);
-//   return toCamelCase(row[0]);
-// };
+export const findUser = async (account_id) => {
+  const reselt = await pools.USER_DATABASE_SQL.query(SQL_QUERIES.FIND_USER_BY_ACCOUNT_ID, [
+    account_id,
+  ]);
+  console.log(reselt);
+  return reselt[0];
+};
 
 export const createUser = async (account_id, password, email) => {
   //const id = uuidv4();
+  // 아이디 uuid
   // 패스워드 암호화 필요
   // 중복 처리 필요
   await pools.USER_DATABASE_SQL.query(SQL_QUERIES.CREATE_USER, [account_id, password, email]);
