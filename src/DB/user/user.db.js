87a8edf830fbb7493889d4c53de1de6cadf6c0e9
ohyudan <ohyudan86@gmail.com>
2024-11-06 15:09:52 +0900

    chore : user.db.js

--- src/DB/user/user.db.js
+++ src/DB/user/user.db.js
@@ -1,24 +1,22 @@
 //import { v4 as uuidv4 } from 'uuid';
 import pools from '../dataBase.js';
 import { SQL_QUERIES } from './user.queries.js';
+//import { toCamelCase } from '../../utils/transformCase.js';
 
 // export const findUserByDeviceID = async (deviceId) => {
 //   const [rows] = await pools.USER_DATABASE_SQL.query(SQL_QUERIES.FIND_USER_BY_DEVICE_ID, [
 //     deviceId,
 //   ]);
-//   return row[0];
+//   return toCamelCase(row[0]);
 // };
 
 export const createUser = async (account_id, password, email) => {
   //const id = uuidv4();
   // 패스워드 암호화 필요
-  const result = await pools.USER_DATABASE_SQL.query(SQL_QUERIES.CREATE_USER, [
-    account_id,
-    password,
-    email,
-  ]);
-  const id = result.insertId;
-  return { id, account_id, password, email };
+  // 중복 처리 필요
+  await pools.USER_DATABASE_SQL.query(SQL_QUERIES.CREATE_USER, [account_id, password, email]);
+
+  return { account_id, password, email };
 };
 
 // export const updateUserLogin = async (id) => {
