2d3f5de1e406751467ea6393bc227ea4fa80561a
ohyudan <ohyudan86@gmail.com>
2024-11-06 16:24:32 +0900

    fix: user.queries.js

--- src/DB/user/user.queries.js
+++ src/DB/user/user.queries.js
@@ -1,5 +1,6 @@
 export const SQL_QUERIES = {
-  FIND_USER_BY_DEVICE_ID: 'SELECT * FROM user WHERE account_id = ?',
+  FIND_USER_BY_ACCOUNT_ID: 'SELECT * FROM accounts WHERE account_id = ?',
+  //FIND_USER_BY_ACCOUNT_ID_PASSWORD: 'SELECT * FROM user WHERE'
   CREATE_USER: 'INSERT INTO accounts (account_id, password, email) VALUES (?, ?, ?)',
-  UPDATE_USER_LOGIN: 'UPDATE user SET last_login = CURRENT_TIMESTAMP WHERE id = ?',
+  //UPDATE_USER_LOGIN: 'UPDATE user SET last_login = CURRENT_TIMESTAMP WHERE id = ?',
 };
