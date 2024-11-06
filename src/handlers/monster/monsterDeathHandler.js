fc8088a9a04353812f6b016a0fa14abc8f3c93ad
hjy2007 <wjddud2007@gmail.com>
2024-11-06 21:09:15 +0900

    enemyMonsterDeathNotification 함수 수정

--- src/handlers/monster/monsterDeathHandler.js
+++ src/handlers/monster/monsterDeathHandler.js
@@ -1,4 +1,5 @@
 import { getProtoMessages } from '../../init/loadProtos.js';
+import { createS2CEnemyMonsterDeathNotification } from '../../utils/monster/monsterNotification/monsterNotification.js';
 import { monsterDeath } from '../../utils/monster/monsterUtils.js';
 
 export const monsterDeathNotification = async (socket, payload) => {
@@ -10,10 +11,13 @@ export const monsterDeathNotification = async (socket, payload) => {
 
     await monsterDeath(uuid, monsterId);
 
-    const protoMessages = getProtoMessages()
-    const monsterData = protoMessages.notification.MonsterData
+    const protoMessages = getProtoMessages();
+    const monsterData = protoMessages.notification.MonsterData;
     const C2SMonsterDeathNotification = {
-        monsterId : monsterId
+      monsterId: monsterId,
+    };
+    const GamePacket = {
+        monsterDeathNotification : C2SMonsterDeathNotification
     }
   } catch (error) {
     throw new Error('몬스터 생성 중 에러 발생', error);
@@ -24,10 +28,11 @@ export const enemyMonsterDeathNotification = async (socket, payload) => {
   try {
     const { monsterId } = payload;
 
-    const uuid = await getUserBySocket(socket);
-    // 데이터에서 uuid가져오기
+    const enemyuuid = await getUserBySocket(socket);
+    // 데이터에서 적의 uuid가져오기
 
-    await monsterDeath(uuid, monsterId);
+
+    EnemyUserSocket.write(createS2CEnemyMonsterDeathNotification(monsterId))
   } catch (error) {
     throw new Error('몬스터 생성 중 에러 발생', error);
   }
