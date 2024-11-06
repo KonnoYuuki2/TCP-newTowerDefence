5661a45b555567eb6a6f8d54a088a82bd1a27f75
hjy2007 <wjddud2007@gmail.com>
2024-11-06 15:40:02 +0900

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-const e = [{ 'monsterId' : 1, 'monsterNumber' : 1}, {'monsterId': 2, 'monsterNumber':2}]
-
-const obj = {'1' : 1, monsterData: [{'key1': 1, 'key2': 2},{'key3': 3, 'key4': 4}]}
-
-console.log(obj['monsterData'])
