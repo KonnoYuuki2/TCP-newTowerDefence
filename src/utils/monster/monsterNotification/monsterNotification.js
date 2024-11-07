b093975f78a56cb097d6abc809f0382cdbb75b91
hjy2007 <wjddud2007@gmail.com>
2024-11-07 15:34:10 +0900

    update: createSocketUtil, monsterNotification
    미완성

--- src/utils/monster/monsterNotification/monsterNotification.js
+++ src/utils/monster/monsterNotification/monsterNotification.js
@@ -1,19 +1,43 @@
 import { PacketType } from '../../../constants/header.js';
-import { getProtoMessages } from '../../../init/loadProtos.js';
 import { maketNotificationPacket } from '../../notification/notification.js';
+import { createDeathMonsterSocket, createSpawnMonsterSocket } from './createSocketUtil.js';
+
+export const createS2CSpawnMonsterNotification = (monsterId, mosnsterNumber) => {
+  const createSpawnMonsterPacket = createSpawnMonsterSocket(monsterId, mosnsterNumber);
+
+  return maketNotificationPacket(createSpawnMonsterPacket, PacketType.SPAWN_MONSTER_RESPONSE);
+};
+
+export const createS2CEnemySpawnMonsterNotification = (monsterId, mosnsterNumber) => {
+  const createSpawnMonsterPacket = createSpawnMonsterSocket(monsterId, mosnsterNumber);
+
+  return maketNotificationPacket(
+    createSpawnMonsterPacket,
+    PacketType.SPAWN_ENEMY_MONSTER_NOTIFICATION,
+  );
+};
 
 export const createS2CEnemyMonsterDeathNotification = (monsterId) => {
-  const protoMessages = getProtoMessages();
-  const enemyMonsterDeathProto = protoMessages.packets.S2CEnemyMonsterDeathNotification;
-
-  const payload = { monsterId };
-
-  const message = enemyMonsterDeathProto.create(payload);
-
-  const enemyMonsterDeathPacket = enemyMonsterDeathProto.encode(message).finish();
+  const enemyMonsterDeathPacket = createDeathMonsterSocket(monsterId);
 
   return maketNotificationPacket(
     enemyMonsterDeathPacket,
     PacketType.ENEMY_MONSTER_DEATH_NOTIFICATION,
   );
 };
+
+// export const createS2CMonsterDeathNotification = (monsterId) => {
+//   const protoMessages = getProtoMessages();
+//   const enemyMonsterDeathProto = protoMessages.packets.S2CEnemyMonsterDeathNotification;
+
+//   const payload = { monsterId };
+
+//   const message = enemyMonsterDeathProto.create(payload);
+
+//   const enemyMonsterDeathPacket = enemyMonsterDeathProto.encode(message).finish();
+
+//   return maketNotificationPacket(
+//     enemyMonsterDeathPacket,
+//     PacketType.ENEMY_MONSTER_DEATH_NOTIFICATION,
+//   );
+// };
