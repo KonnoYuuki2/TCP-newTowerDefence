31707d3e62e6b2fa9ebb32123b56997bd4ba209b
MinjaeK <93378322+KonnoYuuki2@users.noreply.github.com>
2024-11-07 12:34:03 +0900

    Fix: monsterAttackBaseHandler , baseUtils.js
    베이스 몬스터 피격 수정

--- src/handlers/base/monsterAttackBaseHandler.js
+++ src/handlers/base/monsterAttackBaseHandler.js
@@ -5,7 +5,7 @@ export const monsterAttackBaseHandler = async (socket, payload) => {
   try {
     const { damage } = payload;
 
-    await monsterAttackBaseHpVerify(damage);
+    await monsterAttackBaseHpVerify(damage, userId);
   } catch (error) {
     throw new Error(`몬스터 베이스 어택 처리 중 에러 발생`, error);
   }
--- src/utils/base/baseUtils.js
+++ src/utils/base/baseUtils.js
@@ -2,21 +2,14 @@ import { UserFields } from '../../constants/enum.js';
 import pools from '../../DB/dataBase.js';
 import { redis } from '../redis/redis.js';
 
-export const getBaseHp = async (user) => {
-  const baseHp = await redis.getUserField(user.uuid, UserFields.BASE_HP);
+export const getBaseHp = async (userId) => {
+  const baseHp = await redis.getUserField(userId, UserFields.BASE_HP);
 
   return baseHp;
 };
 
-export const monsterAttackBaseHpVerify = async (damage) => {
-  const user = connectedSockets.forEach((value, key) => {
-    return key === socket.id;
-  });
-
-  if (!user) {
-    throw new Error(`유저가 존재하지 않습니다.`);
-  }
-  const baseHp = await getBaseHp(user);
+export const monsterAttackBaseHpVerify = async (damage, userId) => {
+  const baseHp = await getBaseHp(userId);
 
   if (!baseHp) {
     throw new Error(`baseHp가 존재하지 않습니다.`);
@@ -25,7 +18,7 @@ export const monsterAttackBaseHpVerify = async (damage) => {
   baseHp -= damage;
 
   // 유저 정보 업데이트 해주기
-  await redis.updateUserField(user.uuid, UserFields.BASE_HP, baseHp);
+  await redis.updateUserField(userId, UserFields.BASE_HP, baseHp);
 
   console.log(`베이스 피격 정보 업데이트에 성공했습니다.`);
 };
