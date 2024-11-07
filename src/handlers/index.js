8b462003cceba1e206209249bfbceae179d47aea
ohyudan <ohyudan86@gmail.com>
2024-11-07 16:12:12 +0900

    rename : authHandler.js, index.js

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -2,7 +2,7 @@ import HANDLER_IDS from '../constants/handlerIds.js';
 import CustomError from '../utils/error/customError.js';
 import { ErrorCodes } from '../utils/error/errorCodes.js';
 import { matchRequestHandler } from './match/matchHandler.js';
-import { register, login } from './authHander.js';
+import { register, login } from './authHandler.js';
 
 const packetTypes = {
   [HANDLER_IDS.REGISTER_REQUEST]: {
