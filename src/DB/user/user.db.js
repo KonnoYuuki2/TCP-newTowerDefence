61feeb5525a3f2e3509140dc7e7fdc4c5be6ccc3
박지영 <124862122+shd1495@users.noreply.github.com>
2024-11-07 18:24:40 +0900

    fix: 병합 오류 부분 삭제

--- src/DB/user/user.db.js
+++ src/DB/user/user.db.js
@@ -1,6 +1,5 @@
 import pools from '../dataBase.js';
 import { SQL_QUERIES } from './user.queries.js';
-import CustomError from '../../utils/error/customError.js';
 export const findUser = async (account_id) => {
   const reselt = await pools.USER_DATABASE_SQL.query(SQL_QUERIES.FIND_USER_BY_ACCOUNT_ID, [
     account_id,
@@ -9,22 +8,12 @@ export const findUser = async (account_id) => {
   return reselt[0];
 };
 
-export const createUser = async (account_id, password, email) => {
-  const uuid = uuidv4();
-
-  const saltRounds = 10;
-  const newPassword = await bcrypt.hash(password, saltRounds);
-
+export const createUser = async (account_id, uuid, password, email) => {
   const isUserRegistered = await findUser(account_id);
   if (isUserRegistered == null) return Error('유저가 이미 존재함');
 
-  await pools.USER_DATABASE_SQL.query(SQL_QUERIES.CREATE_USER, [
-    account_id,
-    uuid,
-    newPassword,
-    email,
-  ]);
-  return { account_id, uuid, newPassword, email };
+  await pools.USER_DATABASE_SQL.query(SQL_QUERIES.CREATE_USER, [account_id, uuid, password, email]);
+  return { account_id, uuid, password, email };
 };
 
 // export const updateUserLogin = async (id) => {
