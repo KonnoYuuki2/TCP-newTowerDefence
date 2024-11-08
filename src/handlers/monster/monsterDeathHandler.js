54400f64ab048160d4933a79aeb90ba7f09c708d
hjy2007 <wjddud2007@gmail.com>
2024-11-08 16:49:12 +0900

    spawnMonster 방식 변경

--- src/handlers/monster/monsterDeathHandler.js
+++ src/handlers/monster/monsterDeathHandler.js
@@ -30,8 +30,7 @@ export const monsterDeathHandler = async ({ socket, payload }) => {
     // 찾은 socketId로 connectedSockets에 조회하여 찾음
     const enemySocket = connectedSockets.get(socketId);
 
-    enemySocket.write(createResponse(PacketType.MONSTER_DEATH_NOTIFICATION, 0, gamePacket));
-    // 여기 어떤 값을 적어야 하는지 잘 모르겠음
+    enemySocket.write(createResponse(PacketType.ENEMY_MONSTER_DEATH_NOTIFICATION, 0, gamePacket));
   } catch (error) {
     throw new Error('몬스터 Death 처리 중 에러 발생', error);
   }
--- src/handlers/monster/spawnMonsterHandler.js
+++ src/handlers/monster/spawnMonsterHandler.js
@@ -4,47 +4,29 @@ import { spawnMonster } from '../../utils/monster/monsterUtils.js';
 import { redis } from '../../utils/redis/redis.js';
 import { createResponse } from '../../utils/response/createResponse.js';
 
-export const spawnMonsterRequest = async ({ socket, payload }) => {
+export const spawnMonsterRequest = async ({ hostsocket, opposocket }) => {
   try {
-    const { monsterId, monsterNumber } = await spawnMonster(socket);
+    const { monsterId, monsterNumber } = await spawnMonster(hostsocket);
 
-    // 해당 함수에서 socket.id를 받아서 해줌
-    const S2CSpawnMonsterResponse = {
+    const SpawnMonster = {
       monsterId: monsterId,
       monsterNumber: monsterNumber,
     };
+
+    // spawnMonsterResponse 패킷
     const gamePacket = {
-      spawnMonsterResponse: S2CSpawnMonsterResponse,
+      spawnMonsterResponse: SpawnMonster,
     };
 
-    // enemySpawnNotfication 패킷
+    // spawnEnemyMonsterNotification 패킷
     const enemySpawnPacket = {
-      spawnEnemyMonsterNotification: { monsterId: monsterId, monsterNumber: monsterNumber },
+      spawnEnemyMonsterNotification: SpawnMonster,
     };
 
-    const users = await redis.getUsers(socket.gameId);
-
-    let enemyUser;
-    let socketUser;
-
-    users.forEach((user) => {
-      if (user === socket.id) {
-        socketUser = user;
-      } else {
-        enemyUser = user;
-      }
-    });
-
-    // 각 유저의 소켓마다 다른 패킷 전송
-    connectedSockets.forEach((value, key) => {
-      if (key === socketUser) {
-        value.write(createResponse(PacketType.SPAWN_MONSTER_RESPONSE, 0, gamePacket));
-      } else if (key === enemyUser) {
-        value.write(
-          createResponse(PacketType.SPAWN_ENEMY_MONSTER_NOTIFICATION, 0, enemySpawnPacket),
-        );
-      }
-    });
+    hostsocket.write(createResponse(PacketType.SPAWN_MONSTER_RESPONSE, 0, gamePacket));
+    opposocket.write(
+      createResponse(PacketType.SPAWN_ENEMY_MONSTER_NOTIFICATION, 0, enemySpawnPacket),
+    );
   } catch (error) {
     throw new Error('몬스터 생성 요청중 에러 발생', error);
   }
