80dc8cd3af6404a3bb1c256c3adf960a195472da
박지영 <124862122+shd1495@users.noreply.github.com>
2024-11-10 16:56:47 +0900

    fix: 골드 획득이 에셋 정보를 반영하도록 수정

--- src/utils/gameState/gold/goldUtils.js
+++ src/utils/gameState/gold/goldUtils.js
@@ -1,4 +1,5 @@
 import { UserFields } from '../../../constants/constant.js';
+import { getGameAssets } from '../../../init/assets.js';
 import { redis } from '../../redis/redis.js';
 
 export const getUserGold = async (socket) => {
@@ -12,5 +13,7 @@ export const setUserGold = async (socket, gold) => {
 };
 
 export const calculateUserGold = async (gold, level) => {
-  return level * 100 + gold;
+  const { data } = getGameAssets().monsterLevel;
+  const amount = data.find((el) => el.id === level);
+  return amount.reward + gold;
 };
