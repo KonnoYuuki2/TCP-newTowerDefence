b4d9d9da4fdba032c7c626c1ab86050209f87e17
박지영 <124862122+shd1495@users.noreply.github.com>
2024-11-11 20:38:48 +0900

    fix: 강제 종료돼도 기록 저장

--- src/utils/error/errorHandler.js
+++ src/utils/error/errorHandler.js
@@ -1,5 +1,7 @@
 import { PacketType } from '../../constants/header.js';
-import { getOppoSocket, hostSocketWrite } from '../socket/socketUtils.js';
+import pools from '../../DB/dataBase.js';
+import { findUserIdByUUID } from '../../DB/user/user.db.js';
+import { getOppoSocket, getOppoSocketId, hostSocketWrite } from '../socket/socketUtils.js';
 
 export const handleError = async (socket, error) => {
   if (!(await getOppoSocket(socket))) {
@@ -10,6 +12,13 @@ export const handleError = async (socket, error) => {
     const hostOverPacket = {
       gameOverNotification: S2CGameOverNotification,
     };
+    const hostId = await findUserIdByUUID(socket.id);
+
+    const oppoSocketId = await getOppoSocketId(socket);
+
+    const oppoId = await findUserIdByUUID(oppoSocketId);
+
+    await pools.USER_DATABASE_SQL.query(SQL_QUERIES.CREATE_GAME_LOGS, [hostId, oppoId, false]);
 
     await hostSocketWrite(socket, PacketType.GAME_OVER_NOTIFICATION, hostOverPacket);
   } else {
